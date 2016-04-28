e4e1ee512c6a81f846b4ec1824fc21cfccfc77ca
JulianR <juli_fula@hotmail.com>
2016-04-28 01:18:26 -0300

    La umc entiende comandos...

--- UMC/umc.c
+++ UMC/umc.c
@@ -4,5 +4,30 @@
  *  Created on: 28/4/2016
  *      Author: utnso
  */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <commons/string.h>
+#define esIgual(a,b) string_equals_ignore_case(a,b)
 
-
+void main() { //SOCKETS, CONEXION, BLA...
+	char* comando;
+	int velocidad;
+	while (1) {
+		comando = string_new(), scanf("%s", comando);
+		if (esIgual(comando, "retardo")) {
+			printf("velocidad nueva:");
+			scanf("%d", &velocidad);
+			printf("Velocidad actualizada:%d\n", velocidad);
+		}
+		if (esIgual(comando, "dump")) {
+			printf("Estructuras de Memoria\n");
+			printf("Datos de Memoria\n");
+		}
+	if (esIgual(comando,"tlb")) {
+		printf("TLB Borrada :)\n");}
+	if (esIgual(comando, "memoria")) {
+				printf("Memoria Limpiada :)\n");
+			}
+		}
+	}
