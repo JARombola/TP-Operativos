6093391d2f0d7e2f8d8c28d78239540ec322c8bd
JulianR <juli_fula@hotmail.com>
2016-04-28 23:00:05 -0300

    Arreglado error bytes handshake Consola

--- prueba_cliente/cliente.c
+++ prueba_cliente/cliente.c
@@ -13,7 +13,7 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 
-#define PUERTO_SERVIDOR 8080
+#define PUERTO_SERVIDOR 6662
 
 int main(void) {
 	struct sockaddr_in direccServ;
@@ -24,7 +24,7 @@ int main(void) {
 	int cliente = socket(AF_INET, SOCK_STREAM, 0);
 	printf("me cree, estoy en el %d\n", cliente);
 
-	if (connect(cliente, (void*) &direccServ, sizeof(direccServ)) != 0) {
+	if (connect(cliente, (void*) &direccServ, sizeof(direccServ)) == -1) {
 		perror("No se pudo conectar");
 		return 1;
 	}
