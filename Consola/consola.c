2288164068853f0365675bd9b0862e387534275f
Joaquin Aguerrido <joa.ague@gmail.com>
2016-05-03 16:05:15 -0300

    consola: idea de protocolo para imprimir texto

--- Consola/consola.c
+++ Consola/consola.c
@@ -14,6 +14,7 @@
 #include <netinet/in.h>
 
 #define PUERTO_NUCLEO 6662
+int protocolo(int nucleo);
 int conectarNucleo (int, struct sockaddr_in );
 
 int main(int argc, char* argv[]) {//Se le envia por parametro el archivo a ejecutar (#!, ver "Nuevo")
@@ -31,13 +32,37 @@ int main(int argc, char* argv[]) {//Se le envia por parametro el archivo a ejecu
 		conexionNucleo=conectarNucleo(consola_cliente,direccNucleo);}
 		printf("Consola:%d\n", conexionNucleo);
 		//hasta que no se conecta al nucleo no puede mandar mensajes
-		while (1){
+	while (1){
 		char *mensaje;
 		mensaje = string_new();
 		scanf("%s", mensaje);
 		int longitud = htonl(string_length(mensaje));
 		send(consola_cliente, &longitud, sizeof(int32_t), 0);
 		send(consola_cliente, mensaje, strlen(mensaje), 0); //envia datos por teclado*/
+
+		//recibo de nucleo
+
+		switch (protocolo(consola_cliente)) { //misma idea de aceptar clientes nuevos del nucleo
+					case 0:															//Error
+						perror("El nucleo se desconecto\n");
+						close(consola_cliente);
+						//todo intentar conectar? o sin nucleo el programa tiene que terminar forzosamente?
+						break;
+					case 1:														//IMPRIMIR
+						printf("el nucleo quiere que imprima\n");
+
+						break;
+					case 2:														//IMPRIMIR TEXTO
+						printf("el nucleo quiere que imprima texto\n");
+
+						break;
+					}
+				/*	todo luego, tengo otro protocolo de tamanio? o directo recibo lo que tengo que imprimir?
+					char* bufferC = malloc(protocoloC * sizeof(char) + 1);
+					bytesRecibidosC = recv(unaConsola, bufferC, protocoloC, 0);
+					bufferC[protocoloC + 1] = '\0';
+					printf("cliente: %d, me llegaron %d bytes con %s\n", unaConsola,bytesRecibidosC, bufferC);*/
+
 	}
 	return 0;
 }
@@ -59,3 +84,19 @@ if (connect(socket, (void*) &nucleo, sizeof(nucleo))!=0){
 	return socket;
 }
 
+int protocolo(int nucleo) {
+	char* buffer = malloc(1);
+	int bytesRecibidos = recv(nucleo, buffer, 1, 0);
+	buffer[bytesRecibidos] = '\0'; //lo paso a string para comparar
+	if(bytesRecibidos <= 0){ //se desconecto
+		free(buffer);
+		return 0;
+	}
+	if (strcmp("1", buffer) == 0) {//quiere imprimir
+		free(buffer);
+		return 1;
+	} else if (strcmp("2", buffer) == 0) { //quiere imprimir texto
+		free(buffer);
+		return 2;
+	}
+}
